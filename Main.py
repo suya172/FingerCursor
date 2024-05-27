78eac29be31d0cab0b875f796233f4e3a3591e9e
suya172 <suya.in.internet@gmail.com>
2024-05-28 03:23:49 +0900

    autopep8適用

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import pyautogui as gui
 import math
 import time
 
+
 def main():
     cap = cv2.VideoCapture(0)
 
@@ -21,7 +22,7 @@ def main():
     fontpath = 'C:\Windows\Fonts\msgothic.ttc'
     font = ImageFont.truetype(fontpath, 30)
 
-    gui_w ,gui_h =  gui.size()
+    gui_w, gui_h = gui.size()
 
     CURSOR_INTERVAL = 0.1
     CLICK_THRESHOLD = 60
@@ -29,7 +30,7 @@ def main():
     go = True
 
     cursor_time = time.time()
-    prev_distance=999
+    prev_distance = 999
 
     while True:
         success, img = cap.read()
@@ -41,41 +42,49 @@ def main():
         img.flags.writeable = True
         if results.multi_hand_landmarks:
             for hand_landmark in results.multi_hand_landmarks:
-            
-                for id,landmark in enumerate(hand_landmark.landmark):
-                    cv_h,cv_w, _ = img.shape
 
-                    #カーソル移動用
-                    if id == 8: #人差し指の先端
-                        #モニター上の座標を計算
-                        cursor_monitor_x, cursor_monitor_y = int(landmark.x*gui_w), int(landmark.y*gui_h)
-                        #描画
-                        cursor_cv_x, cursor_cv_y = int(landmark.x*cv_w), int(landmark.y*cv_h)
-                        cv2.drawMarker(img, (cursor_cv_x, cursor_cv_y), (0, 255, 255), markerType=cv2.MARKER_CROSS, markerSize=10, thickness=5)
+                for id, landmark in enumerate(hand_landmark.landmark):
+                    cv_h, cv_w, _ = img.shape
 
-                    #マウス判定用
-                    if id == 4: #親指の先端
-                        a_cv_x , a_cv_y = int(landmark.x*cv_w), int(landmark.y*cv_h)
-                        cv2.drawMarker(img, (a_cv_x, a_cv_y), (192, 192, 192), markerType=cv2.MARKER_CROSS, markerSize=8, thickness=5)
-                    if id == 12: #中指の先端
-                        un_cv_x , un_cv_y = int(landmark.x*cv_w), int(landmark.y*cv_h)
-                        cv2.drawMarker(img, (un_cv_x, un_cv_y), (0, 0, 255), markerType=cv2.MARKER_CROSS, markerSize=8, thickness=5)
+                    # カーソル移動用
+                    if id == 8:  # 人差し指の先端
+                        # モニター上の座標を計算
+                        cursor_monitor_x, cursor_monitor_y = int(
+                            landmark.x*gui_w), int(landmark.y*gui_h)
+                        # 描画
+                        cursor_cv_x, cursor_cv_y = int(
+                            landmark.x*cv_w), int(landmark.y*cv_h)
+                        cv2.drawMarker(img, (cursor_cv_x, cursor_cv_y), (0, 255, 255),
+                                       markerType=cv2.MARKER_CROSS, markerSize=10, thickness=5)
 
-                #描画
-                cv2.line(img,(a_cv_x,a_cv_y),(un_cv_x,un_cv_y),(0,0,0),thickness=1,lineType=cv2.LINE_8)
+                    # マウス判定用
+                    if id == 4:  # 親指の先端
+                        a_cv_x, a_cv_y = int(
+                            landmark.x*cv_w), int(landmark.y*cv_h)
+                        cv2.drawMarker(img, (a_cv_x, a_cv_y), (192, 192, 192),
+                                       markerType=cv2.MARKER_CROSS, markerSize=8, thickness=5)
+                    if id == 12:  # 中指の先端
+                        un_cv_x, un_cv_y = int(
+                            landmark.x*cv_w), int(landmark.y*cv_h)
+                        cv2.drawMarker(img, (un_cv_x, un_cv_y), (0, 0, 255),
+                                       markerType=cv2.MARKER_CROSS, markerSize=8, thickness=5)
 
-                #距離を計算
+                # 描画
+                cv2.line(img, (a_cv_x, a_cv_y), (un_cv_x, un_cv_y),
+                         (0, 0, 0), thickness=1, lineType=cv2.LINE_8)
+
+                # 距離を計算
                 distance = math.sqrt((a_cv_x-un_cv_x)**2+(a_cv_y-un_cv_y)**2)
 
                 img_pil = Image.fromarray(img)
                 draw = ImageDraw.Draw(img_pil)
-                draw.text((10, 5), str(distance),  fill=(0, 0, 255), font=font,  stroke_width=1, stroke_fill=(0, 0, 255)) 
+                draw.text((10, 5), str(distance),  fill=(0, 0, 255),
+                          font=font,  stroke_width=1, stroke_fill=(0, 0, 255))
                 img = np.array(img_pil)
 
-                
                 if go:
                     if (time.time() - cursor_time) > CURSOR_INTERVAL:
-                        gui.moveTo(cursor_monitor_x,cursor_monitor_y)
+                        gui.moveTo(cursor_monitor_x, cursor_monitor_y)
                         cursor_time = time.time()
 
                     if (prev_distance < CLICK_THRESHOLD) & (distance >= CLICK_THRESHOLD):
@@ -83,8 +92,6 @@ def main():
                         prev_distance = distance
                     elif (prev_distance >= CLICK_THRESHOLD) & (distance < CLICK_THRESHOLD):
                         prev_distance = distance
-                        
-
 
                 # mpDraw.draw_landmarks(
                 #     img,
@@ -99,6 +106,7 @@ def main():
             break
     gui.mouseUp(button='left')
     cap.release()
+
 
 if __name__ == '__main__':
     main()
